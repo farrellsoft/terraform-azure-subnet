fd94b0d0010ac083bb8264acd00edcb22e409004
Jason Farrell <imjason@gmail.com>
2023-02-19 21:48:34 -0500

    added outputs

---
+++ outputs.tf
@@ -0,0 +1,10 @@
+
+output name {
+  value       = azurerm_subnet.this.name
+  description = "The name of the subnet."
+}
+
+output id {
+  value       = azurerm_subnet.this.id
+  description = "The ID of the subnet."
+}
